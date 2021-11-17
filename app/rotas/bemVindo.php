e8c351cdcc45f89ca6de3cd1590a1d0f9f71914c
Gustavo Lobo <gustavoqe.75@gmail.com>
2021-11-17 17:21:45 -0300

    Delete bemVindo.php

--- app/rotas/bemVindo.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// SAÍDA DO CLIENTE
-echo $twig->render('bemVindo.html', [
-    'titulo' => 'Seja muito bem vindo doador !!! ',
-    'informacao' => 'Muito obrigado por contribuir com o nosso projeto, muitas pessoas agradecem !!!'
-]);
